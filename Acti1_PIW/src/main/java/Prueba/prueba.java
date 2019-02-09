5764ae19aecb309a9d1cd125332e328a6df427e8
BOG-A209-E-018 <BOG-A209-E-018@RGH-LS6-18.UMD.LOCAL.EDU>
2019-02-09 08:03:26 -0500

    Commit, tiempo

---
+++ Acti1_PIW/src/main/java/Prueba/prueba.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Prueba;
+
+import org.joda.time.LocalTime;
+
+
+/**
+ *
+ * @author BOG-A209-E-018
+ */
+public class prueba {
+    public static void main (String [] args){
+        LocalTime tiempo= new LocalTime ();
+        System.out.println("El tiempo real es: "+ tiempo);
+    }
+    
+}
